0603d29dd7ae88b2b65cb4bc45571d3bccdd2e8b
Jakub Mestek <jakub.mestek@outlook.cz>
2020-04-13 18:24:23 +0200

    comment on duration units

--- java/OzobotsCPF/src/main/java/ozobotscpf/actions/Action.java
+++ java/OzobotsCPF/src/main/java/ozobotscpf/actions/Action.java
@@ -3,6 +3,10 @@ package ozobotscpf.actions;
 import ozobotscpf.nodes.AgentMapNode;
 
 public interface Action {
+    /**
+     * Duration of the action in seconds.
+     * @return duration of the action in seconds
+     */
     double getDuration();
     void apply(AgentMapNode agent, double deltaT);
 }
