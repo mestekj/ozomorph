1c783aedcf1c65f0c7d50d8f98be00998cfe5fd6
Jakub Mestek <jakub.mestek@outlook.cz>
2020-05-02 14:29:50 +0200

    Actions duration set to 1s.

--- java/OzobotsCPF/src/main/java/ozobotscpf/actions/ActionFactory.java
+++ java/OzobotsCPF/src/main/java/ozobotscpf/actions/ActionFactory.java
@@ -4,13 +4,13 @@ public class ActionFactory {
     public Action createAction(String picatAction){
         switch (picatAction){
             case "goAhead":
-                return new MoveAction(2);
+                return new MoveAction(1);
             case "turnLeft":
-                return new TurnLeftAction(2);
+                return new TurnLeftAction(1);
             case "turnRight":
-                return new TurnRightAction(2);
+                return new TurnRightAction(1);
             case "wait":
-                return new WaitAction(2);
+                return new WaitAction(1);
             default:
                 throw new IllegalArgumentException(String.format("Unknown picat action: %s.", picatAction));
         }
