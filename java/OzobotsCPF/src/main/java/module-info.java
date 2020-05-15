9acaa147647d4ad3280669699e9a601ec148506f
Jakub Mestek <jakub.mestek@outlook.cz>
2020-05-15 13:16:55 +0200

    logback configuration file

--- java/OzobotsCPF/src/main/java/module-info.java
+++ java/OzobotsCPF/src/main/java/module-info.java
@@ -4,6 +4,8 @@ module ozobotscpf {
     requires org.slf4j;
     //only runtime dependency, but javafx-maven-plugin does not allow to manually add it to module-path
     requires ch.qos.logback.classic;
+    //needed by logback if configuration file is used
+    requires java.naming;
 
     requires jdom2;
     requires java.xml;
