51069e302977bfe2360c3d4fc5e19cb533cedb3d
mestekj <55023124+mestekj@users.noreply.github.com>
2025-11-25 11:54:53 +0100

    fix python templates

--- ozocode_templates/python_template.py
+++ ozocode_templates/python_template.py
@@ -2,6 +2,7 @@ import ozobot
 import asyncio
 from _ozo import DeviceManager
 from _ozo import get_robot
+import math
 
 # To be inserted
 agents = ...
@@ -17,14 +18,14 @@ async def execute_action(action):
     if action == 'goAhead':
         await dm.navigation.anavigate(ozobot.Directions.FORWARD, follow = True)
     elif action == 'turnLeft':
-        await dm.navigation.anavigate(ozobot.Directions.LEFT, follow = False)
+        await dm.movement.arotate(90 * (math.pi / 180), 90 * (math.pi / 180))
     elif action == 'turnRight':
-        await dm.navigation.anavigate(ozobot.Directions.RIGHT, follow = False)
+        await dm.movement.arotate(- 90 * (math.pi / 180), 90 * (math.pi / 180))
     elif action == 'wait':
         await asyncio.sleep(1)
 
 async def set_light_color(r, g, b):
-    await dm.light_effects.aset_light_color_rgb(r / 127, g / 127, b / 127, ozobot.Lights.TOP | ozobot.Lights.FRONT_1 | ozobot.Lights.FRONT_3 | ozobot.Lights.FRONT_5)
+    await dm.light_effects.aset_light_color_rgb(r / 255, g / 255, b / 255, ozobot.Lights.TOP | ozobot.Lights.FRONT_1 | ozobot.Lights.FRONT_3 | ozobot.Lights.FRONT_5)
 
 async def main():
     async with asyncio.TaskGroup() as _tg:
@@ -35,6 +36,7 @@ async def main():
 
             with dm.use_device(device_name):
                 await set_light_color(r, g, b)
+                await dm.navigation.aset_line_following_speed(50 / 1000.0)
                 for action in plan:
                     await execute_action(action)
 
